9f9809417e22c9987fe05fff43551b4afa47e73e
miaoyu <miaoyu@sefonsoft.com>
2020-11-23 15:33:47 +0800

    add command args

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,27 @@
 const chalk = require("chalk");
-const createServer = require("./createServer")
-const [ , ,...args] = process.argv 
-const port = args[0] || 4000
+const createServer = require("./createServer");
+const [, , ...args] = process.argv;
+let port = 4000;
+
+for (let i = 0; i < args.length; ++i) {
+  const arg = args[i]
+  if (arg === "--port") {
+    port = args[i + 1];
+    ++i
+    continue
+  }
+  if (arg === "--help") {
+    console.log(
+      chalk.blue(`
+        --port server start port 
+      `)
+    );
+    return
+  }
+}
+
 createServer().listen(port, () => {
-  console.log(chalk.green(`Server start ${port} prot http://localhost:${port}`));
+  console.log(
+    chalk.green(`Server start ${port} prot http://localhost:${port}`)
+  );
 });
